553f62eb88fd01e5e1b292f010411c7f89949335
Rodrigo Mungo <rodrigomungo@gmail.com>
2023-07-03 01:57:16 -0300

    refactor: improve distance service

--- BLL.TrackingAdmin/Services/DistanceService.cs
+++ BLL.TrackingAdmin/Services/DistanceService.cs
@@ -13,7 +13,6 @@ namespace BLL.TrackingAdmin.Services
     {
         private readonly IMapper _mapper;
         private readonly IUnitOfWork _unitOfWork;
-        private readonly Random _random = new Random();
 
         public DistanceService(IUnitOfWork unitOfWork, IMapper mapper)
         {
@@ -36,7 +35,68 @@ namespace BLL.TrackingAdmin.Services
             locations.Insert(0, originId);
             List<int> indexes = locations.Select(d => locationModels.FindIndex(c => c.Id == d)).ToList();
 
-            // Matriz completa de distancias
+            long[][] distanceMatrix = GetDistanceMatrix(distanceModels, locationModels, indexes);
+
+            // Create the traveling salesman problem
+            RoutingIndexManager manager = new RoutingIndexManager(distanceMatrix.GetLength(0), 1, 0);
+            RoutingModel routing = new RoutingModel(manager);
+
+            int transitCallbackIndex = routing.RegisterTransitCallback((long i, long j) => distanceMatrix[manager.IndexToNode(i)][manager.IndexToNode(j)]);
+
+            // Define the cost of each arc
+            routing.SetArcCostEvaluatorOfAllVehicles(transitCallbackIndex);
+
+            // Set the search parameters
+            RoutingSearchParameters searchParameters = operations_research_constraint_solver.DefaultRoutingSearchParameters();
+            searchParameters.FirstSolutionStrategy = FirstSolutionStrategy.Types.Value.PathCheapestArc;
+            searchParameters.LocalSearchMetaheuristic = LocalSearchMetaheuristic.Types.Value.GuidedLocalSearch;
+            searchParameters.TimeLimit = new Duration { Seconds = 1 };
+
+            // Resolve the problem
+            Assignment solution = routing.SolveWithParameters(searchParameters);
+
+            // Create the list of distances of the best course
+            List<Distance> bestRoute = GetBestRouteBySolution(locationModels, distanceModels, indexes, manager, routing, solution);
+            return bestRoute;
+        }
+
+        private List<Distance> GetBestRouteBySolution(List<LocationModel> locationModels, List<DistanceModel> distanceModels, List<int> indexes, RoutingIndexManager manager, RoutingModel routing, Assignment solution)
+        {
+            List<DistanceModel> bestRoute = new List<DistanceModel>();
+            if (solution != null)
+            {
+                long index = routing.Start(0);
+                while (!routing.IsEnd(index))
+                {
+                    long nextIndex = solution.Value(routing.NextVar(index));
+                    LocationModel originLocation = locationModels[indexes[manager.IndexToNode((int)index)]];
+                    LocationModel destinationLocation = locationModels[indexes[manager.IndexToNode((int)nextIndex)]];
+                    DistanceModel distance = distanceModels.First(d => d.OriginLocationId == originLocation.Id && d.DestinationLocationId == destinationLocation.Id);
+                    bestRoute.Add(distance);
+                    index = nextIndex;
+                }
+            }
+            List<Distance> distances = _mapper.Map<List<Distance>>(bestRoute);
+            return distances;
+        }
+
+        private static long[][] GetDistanceMatrix(List<DistanceModel> distanceModels, List<LocationModel> locationModels, List<int> locationIndexes)
+        {
+            long[][] fullDistanceMatrix = GetFullDistanceMatrix(distanceModels, locationModels);
+            long[][] smallDistanceMatrix = new long[locationIndexes.Count][];
+            for (int i = 0; i < locationIndexes.Count; i++)
+            {
+                smallDistanceMatrix[i] = new long[locationIndexes.Count];
+                for (int j = 0; j < locationIndexes.Count; j++)
+                {
+                    smallDistanceMatrix[i][j] = fullDistanceMatrix[locationIndexes[i]][locationIndexes[j]];
+                }
+            }
+            return smallDistanceMatrix;
+        }
+
+        private static long[][] GetFullDistanceMatrix(List<DistanceModel> distanceModels, List<LocationModel> locationModels)
+        {
             int n = locationModels.Count;
             long[][] fullDistanceMatrix = new long[n][];
             for (int i = 0; i < locationModels.Count; i++)
@@ -58,55 +118,7 @@ namespace BLL.TrackingAdmin.Services
                     }
                 }
             }
-
-            // Matriz reducida de distancias
-            long[][] smallDistanceMatrix = new long[indexes.Count][];
-            for (int i = 0; i < indexes.Count; i++)
-            {
-                smallDistanceMatrix[i] = new long[indexes.Count];
-                for (int j = 0; j < indexes.Count; j++)
-                {
-                    smallDistanceMatrix[i][j] = fullDistanceMatrix[indexes[i]][indexes[j]];
-                }
-            }
-
-            // Crea el problema del viajante de comercio.
-            RoutingIndexManager manager = new RoutingIndexManager(smallDistanceMatrix.GetLength(0), 1, 0);
-            RoutingModel routing = new RoutingModel(manager);
-
-            int transitCallbackIndex = routing.RegisterTransitCallback((long i, long j) => smallDistanceMatrix[manager.IndexToNode(i)][manager.IndexToNode(j)]);
-
-            // Define el costo de cada arco.
-            routing.SetArcCostEvaluatorOfAllVehicles(transitCallbackIndex);
-
-            // Establece las opciones de búsqueda.
-            RoutingSearchParameters searchParameters = operations_research_constraint_solver.DefaultRoutingSearchParameters();
-            searchParameters.FirstSolutionStrategy = FirstSolutionStrategy.Types.Value.PathCheapestArc;
-            searchParameters.LocalSearchMetaheuristic = LocalSearchMetaheuristic.Types.Value.GuidedLocalSearch;
-            searchParameters.TimeLimit = new Duration { Seconds = 1 };
-
-            // Resuelve el problema.
-            Assignment solution = routing.SolveWithParameters(searchParameters);
-
-            // Crea la lista de distancias del mejor recorrido
-            List<DistanceModel> bestRoute = new List<DistanceModel>();
-            if (solution != null)
-            {
-                long index = routing.Start(0);
-                while (!routing.IsEnd(index))
-                {
-                    long nextIndex = solution.Value(routing.NextVar(index));
-                    LocationModel originLocation = locationModels[indexes[manager.IndexToNode((int)index)]];
-                    LocationModel destinationLocation = locationModels[indexes[manager.IndexToNode((int)nextIndex)]];
-                    DistanceModel distance = distanceModels.First(d => d.OriginLocationId == originLocation.Id && d.DestinationLocationId == destinationLocation.Id);
-                    bestRoute.Add(distance);
-                    index = nextIndex;
-                }
-            }
-
-            // Mapea las distancias
-            List<Distance> distances = _mapper.Map<List<Distance>>(bestRoute);
-            return distances;
+            return fullDistanceMatrix;
         }
     }
 }
